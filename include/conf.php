b7cc0077d9e2406e00007a31c5ff485e731260f4
Leif Neve <leifneve@gmail.com>
2023-11-06 09:42:27 -0500

    Update conf.php

--- include/conf.php
+++ include/conf.php
@@ -1,5 +1,5 @@
 <?php
-  // NOTE: These programs require an NLM/NCBI E-utilities key:
+  // NOTE: Babelmesh require an NLM/NCBI E-utilities key:
   // https://ncbiinsights.ncbi.nlm.nih.gov/2017/11/02/new-api-keys-for-the-e-utilities/
   $ncbi_key = "";
   // Supply a Google recAPTCHA key to limit spam access. 
